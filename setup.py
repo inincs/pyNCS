01cd32223842c28b7bef96cdee505ec1f70c048c
Sadique Sheik <sheiksadique@gmail.com>
2013-07-16 12:30:10 +0200

    updated version and description

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 from distutils.core import setup
 
 setup(name='pyNCS',
-	version='10.09beta',
-	description='inincs/pyNCS',
+	version='13.07beta',
+	description='inincs/pyNCS headless',
 	author='Sadique Sheik, Emre Neftci, Fabio Stefanini, Giacomo Indiveri',
 	author_email='sadique@ini.phys.ethz.ch, emre@ini.phys.ethz.ch, giacomo@ini.phys.ethz.ch, fabios@ini.phys.ethz.ch',
 	url='https://github.com/inincs/pyNCS',
